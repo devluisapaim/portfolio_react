b1f0ca9d7eb1ae8afe4468a42b11cebd1aeaa495
luisacpaim <luisac.paim@hotmail.com>
2023-01-18 18:18:57 -0300

    adicionando Certificado PretaLab

--- src/services/listCertificates.js
+++ src/services/listCertificates.js
@@ -1,6 +1,7 @@
 import Startech from "../assets/startech.png";
 import Iniciante from "../assets/one_iniciante_1.png";
 import Frontend from "../assets/one_frontent_1.png";
+import Pretalab from "../assets/pretalab.png"
 
 export const listCertificates = [
     {
@@ -8,7 +9,7 @@ export const listCertificates = [
         name: "Full Stack - Gama Academy",
         img: Startech,
         description: "Programa de Bolsas Startech. Treinamento Full Stack, promovido pela Gama Academy",
-        date: "27 de julho à 29 de agosto de 2021",
+        date: "27 de julho a 29 de agosto de 2021",
         duration: "120 horas",
         linkCertificate: "https://drive.google.com/file/d/1Im6G5fVFDL8rzKIsqmCN6nVx_QANmLg3/view?usp=sharing"
     },
@@ -29,16 +30,16 @@ export const listCertificates = [
         date: "",
         duration: "104 horas",
         linkCertificate: "https://cursos.alura.com.br/user/luisapaimm/degree-front-end-turma3-one-431390/certificate"
+    },
+    {
+        id: 4,
+        name: "Ciclo Formativo - PretaLab",
+        img: Pretalab,
+        description: "Ciclo de Formação Frontend: HTML/CSS, JavaScript, React.JS",
+        date: "23 de agosto a 14 de dezembro de 2022",
+        duration: "110 horas",
+        linkCertificate: "https://drive.google.com/file/d/1dBVsDWFN9UteB_tV1vvbC9dpnVfDKWE9/view?usp=share_link"
     }
-    // {
-    //     id: 4,
-    //     name: "Ciclo Formativo - PretaLab",
-    //     img: ,
-    //     description: "Ciclo de Formação Frontend: HTML/CSS, JavaScript, React.JS",
-    //     date: ,
-    //     duration: ,
-    //     linkCertificate:
-    // },
     // {
     //     id: 5,
     //     name: ,
